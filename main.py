981f5792328fe5f43c3d80cef2b820ac96f565a8
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-10-23 11:17:45 -0300

    adding cities data

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 # Load the data
-data = pd.read_csv('data.csv')
+data_names = pd.read_csv('br_ibge_nomes_brasil.csv')
 
-# Print the data
-print(data)
+data_locals = pd.read_csv('br_bd_diretorios_brasil_municipio.csv')
+
+# Print the first 10 names
+for line in range(0, 10):
+    print(data_names['nome'][line])
+    print(data_locals['nome'][line])
