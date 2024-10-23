67f635846cd80aa33d5f8e6dfa711f2bc14a5253
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-10-23 13:40:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ data_names = pd.read_csv('br_ibge_nomes_brasil.csv')
 
 data_locals = pd.read_csv('br_bd_diretorios_brasil_municipio.csv')
 
+#criar tabela que relaciona os nomes com os locais
+
 # Print the first 10 names
 for line in range(0, 10):
     print(data_names['nome'][line])
