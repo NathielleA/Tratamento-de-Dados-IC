f8a76ef20947ac14fe75a580fee64a30cf31c663
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-10-23 15:26:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from gender_guesser_br import Genero
 
 # Load the data
 data_names = pd.read_csv('br_ibge_nomes_brasil.csv')
@@ -6,8 +8,19 @@ data_names = pd.read_csv('br_ibge_nomes_brasil.csv')
 data_locals = pd.read_csv('br_bd_diretorios_brasil_municipio.csv')
 
 #criar tabela que relaciona os nomes com os locais
+main_df = pd.merge(data_names, data_locals, on='id_municipio')
+
+print(main_df.loc[20, 'nome_x'])
+print(main_df.loc[20, 'nome_y'])
+print(main_df.loc[20, 'nome_uf'])
+print(main_df.loc[20, 'nome_regiao'])
+print(main_df.loc[20, 'quantidade_nascimentos_ate_2010'])
 
 # Print the first 10 names
-for line in range(0, 10):
-    print(data_names['nome'][line])
-    print(data_locals['nome'][line])
+#for line in range(0, 10):
+    # nome = data_names['nome'][line]
+    # genero = Genero(nome)
+    # print(nome, genero())
+    # print(data_locals['nome'][line])'
+
+
