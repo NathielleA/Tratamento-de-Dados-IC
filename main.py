5e13ab457f82d14770a2de50f15bf6ef5494be6f
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-10-28 12:04:35 -0300

    Att

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ names_locals_df = pd.merge(data_names, data_locals, on='id_municipio')
 # select only the columns that are needed for the final table
 name_location_table = names_locals_df[['nome_x', 'nome_y', 'nome_uf', 'nome_regiao', 'quantidade_nascimentos_ate_2010']]
 
+# Agrupar por 'nome' e 'nome_regiao', e somar a coluna 'quantidade_nascimentos'
+grouped_df = name_location_table.groupby(['nome_x', 'nome_regiao'])['quantidade_nascimentos_ate_2010'].sum().reset_index()
+
 #create a new colunm for the gender of the names
 name_location_table['genero'] = None
 
-# associate the names to the gender
-for line in range(0, len(name_location_table)):
-    name = name_location_table['nome_x'][line]
-    genero = Genero(name)
-    name_location_table.loc[line, 'genero'] = genero()
+# # associate the names to the gender
+# for line in range(0, 10):
+#     name = name_location_table['nome_x'][line]
+#     genero = Genero(name)
+#     name_location_table.loc[line, 'genero'] = genero()
 
-name_location_table.to_csv('nomes_locais.csv', sep=',', index=False)
+grouped_df.to_csv('nomes_locais.csv', sep=',', index=False)
 
